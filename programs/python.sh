12a97df97f4a8d851ff10bcd3956d137d4c2b20d
Leslie <leslie.clif@gmail.com>
2021-02-07 14:35:55 +0530

    adding docker-compose

--- programs/python.sh
+++ programs/python.sh
@@ -4,4 +4,4 @@
 echo "🐍 Installing Python"
 sudo apt install -qq -y python3-venv python3-pip python3-setuptools
 sudo pip3 install --upgrade pip
-sudo pip3 install python-vagrant mkdocs ansible
+sudo pip3 install python-vagrant mkdocs ansible docker-compose
